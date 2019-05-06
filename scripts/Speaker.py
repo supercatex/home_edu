5740c4807b9e5502c3bccbc7a9a123ae0a24d165
Kinda Lam <lamkinun@gmail.com>
2019-05-06 11:42:47 +0800

    Speaker

---
+++ scripts/Speaker.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import pyttsx3
+
+
+class Speaker(object):
+
+	def __init__(self, speed=100, volume=1.0, voice_id=2):
+		self.engine = pyttsx3.init()
+		self.set_speed(speed)
+		self.set_volume(volume)
+		self.set_voice(voice_id)
+	
+	def set_speed(self, speed):
+		self.engine.setProperty("rate", speed)
+	
+	def set_volume(self, volume):
+		self.engine.setProperty("volume", volume)
+	
+	def set_voice(self, voice_id):
+		voices = self.engine.getProperty("voices")
+		print(len(voices))
+		if voice_id >= 0 and voice_id < len(voices):
+			voice = voices[voice_id]
+			self.engine.setProperty("voice", voice.id)
+		else:
+			print("No voice id: ", voice_id)
+	
+	def say(self, msg, log=False):
+		self.engine.say(msg)
+		self.engine.runAndWait()
+
+
+if __name__ == "__main__":
+	for i in range(69):
+		print(i)
+		P = Speaker(voice_id=i)
+		P.say("Hello, my name is John!")
+		# 2, 9, 10, 11, 14, 15, 17, 22
