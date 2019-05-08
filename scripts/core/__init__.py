eeffde4c85d76dd1211de8996abadce91f67deb7
Kinda Lam <lamkinun@gmail.com>
2019-05-08 15:55:52 +0800

    Chassis

--- scripts/core/__init__.py
+++ scripts/core/__init__.py
@@ -4,6 +4,7 @@ from .ROS_Topic import *
 from .Astra import Astra
 from .Dynamixel import Dynamixel
 from .Kobuki import Kobuki
+from .RobotChassis import RobotChassis
 from .Manipulator import Manipulator
 from .RobotArm import RobotArm
 from .Speaker import Speaker
