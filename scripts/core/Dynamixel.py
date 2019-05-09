48b6ed0ebced7310312bd4f4f5c95f65fadc1d13
Kinda Lam <lamkinun@gmail.com>
2019-05-09 12:02:54 +0800

    reformat code.

--- scripts/core/Dynamixel.py
+++ scripts/core/Dynamixel.py
@@ -1,67 +1,90 @@
 #!/usr/bin/env python
-import rospy
-from ROS_Topic import ROS_Topic_Dynamixel as T
-from std_msgs.msg import Float64
-from dynamixel_controllers.srv import SetSpeed
-from dynamixel_msgs.msg import JointState
-import math
 import time
 
+import math
+import rospy
+from ROS_Topic import ROS_Topic_Dynamixel as T
+from dynamixel_controllers.srv import SetSpeed
+from dynamixel_msgs.msg import JointState
+from std_msgs.msg import Float64
 
 '''
-roslaunch rchomeedu_arm arm.launch
+# Default ROS command.
+roslaunch home_edu arm.launch
 '''
+
 
 class Dynamixel(object):
 
-	def __init__(self, name="tilt_controller"):
-		self.topic = T(name)
-		self.state = None
-		
-		self.publisher = rospy.Publisher(
-			self.topic.command(),
-			Float64,
-			queue_size = 1,
-			latch = True
-		)
-		
-		self.state_subscriber = rospy.Subscriber(
-			self.topic.state(),
-			JointState,
-			self.state_callback
-		)
-		
-		rospy.wait_for_service(self.topic.set_speed())
-		self.set_speed = rospy.ServiceProxy(
-			self.topic.set_speed(),
-			SetSpeed
-		)
-		
-		while self.state is None: pass
-		print("Dynamixel (%s) is OK." % self.topic.name)
-	
-	def set_radian(self, radian):
-		self.publisher.publish(radian)
-	
-	def set_speed(self, speed):
-		self.set_speed(speed)
-	
-	def state_callback(self, data):
-		self.state = data
+    # Constructor:
+    def __init__(self, topic_name="tilt_controller"):
+
+        # Dynamixel ROS topic object.
+        self.topic = T(topic_name)
+
+        # Dynamixel information.
+        self.state = None
+
+        # Dynamixel command publisher.
+        self.publisher = rospy.Publisher(
+            self.topic.command(),
+            Float64,
+            queue_size=1,
+            latch=True
+        )
+
+        # Dynamixel state subscriber.
+        self.state_subscriber = rospy.Subscriber(
+            self.topic.state(),
+            JointState,
+            self.state_callback
+        )
+
+        # Dynamixel speed service.
+        rospy.wait_for_service(self.topic.set_speed())
+        self.set_speed = rospy.ServiceProxy(
+            self.topic.set_speed(),
+            SetSpeed
+        )
+
+        rospy.loginfo("Waiting %s topic callback..." % self.topic.name)
+        rospy.wait_for_message(self.topic.state(), JointState)
+        rospy.loginfo("Dynamixel (%s) is OK." % self.topic.name)
+
+    # Change Dynamixel angle.
+    def set_radian(self, radian):
+        self.publisher.publish(radian)
+
+    # Change Dynamixel moving speed.
+    def set_speed(self, speed):
+        self.set_speed(speed)
+
+    # Dynamixel ROS state topic callback.
+    def state_callback(self, data):
+        self.state = data
 
 
+# How to use?
 if __name__ == "__main__":
-	rospy.init_node("home_edu_dynamixel", anonymous=True)
-	rate = rospy.Rate(20)
-	servo = Dynamixel("waist_controller")
-	servo.set_speed(0.3)
-	if servo.state.current_pos > 1:
-		servo.set_radian(0)
-	else:
-		servo.set_radian(math.pi / 4)
-	
-	time.sleep(1)
-	while servo.state.is_moving:
-		print(servo.state)
-		rate.sleep()
-
+    rospy.init_node("home_edu_dynamixel", anonymous=True)
+    rate = rospy.Rate(20)
+    
+    # 1. Create a Dynamixel object.
+    servo = Dynamixel("waist_controller")
+    
+    # 2. Set default speed.
+    servo.set_speed(0.3)
+    
+    # 3. Set angle.
+    if servo.state.current_pos >= 0.01:
+        servo.set_radian(0)
+    else:
+        servo.set_radian(math.pi / 4)
+    
+    # 4. Set a delay to wait state change to is moving.
+    time.sleep(0.2)
+    
+    # 5. Display state information on the screen.
+    while servo.state.is_moving:
+        rospy.loginfo(servo.state)
+        rate.sleep()
