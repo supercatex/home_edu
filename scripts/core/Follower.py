56413ed7db3130d5b8ae026af9f112b59f14f4c0
Kinda Lam <lamkinun@gmail.com>
2019-05-23 16:42:36 +0800

    Follower

--- scripts/core/Follower.py
+++ scripts/core/Follower.py
@@ -8,16 +8,22 @@ import numpy as np
 
 class Follower(object):
     
-    def __init__(self, topic_name="camera", target_distance=600, PID=(1.0, 0.0, 0.0)):
+    def __init__(self, topic_name="camera", target_distance=600, PID_1=(1.0, 0.0, 0.0), PID_2=(1.0, 0.0, 0.0)):
         self.camera = Astra(topic_name)
         self.chassis = Kobuki()
         self.target_distance = target_distance
-        self.PID = PID
-        self.iter_error = 0.0
-        self.last_error = 0.0
+
+        self.PID_1 = PID_1
+        self.iter_error_1 = 0.0
+        self.last_error_1 = 0.0
+
+        self.PID_2 = PID_2
+        self.iter_error_2 = 0.0
+        self.last_error_2 = 0.0
         
     def next_step(self):
         distance = 0
+        offset_z = 0
         x = 0
         z = 0
         
@@ -25,16 +31,32 @@ class Follower(object):
         indices = np.nonzero(depth_image)
         if len(indices[0]) > 0:
             distance = np.min(depth_image[indices])
+            n = np.where(depth_image == distance)
+            offset_z = n[1][0]
         
-        if distance == 0 or distance > 1500:
+        if distance == 0 or distance > 800:
             return x, z
         
         error = distance - self.target_distance
-        delta_error = error - self.last_error
-        x = self.PID[0] * error + self.PID[1] * self.iter_error + self.PID[2] * delta_error
-        self.iter_error += error
-        self.last_error = error
-        
+        if abs(error) < 10:
+            error = 0
+            self.iter_error_1 = 0
+        delta_error = error - self.last_error_1
+        x = self.PID_1[0] * error + self.PID_1[1] * self.iter_error_1 + self.PID_1[2] * delta_error
+        self.iter_error_1 += error
+        self.last_error_1 = error
+        x = max(min(x, 0.5), -0.5)
+
+        error = 320 - offset_z
+        if abs(error) < 50:
+            error = 0
+            self.iter_error_2 = 0
+        delta_error = error - self.last_error_2
+        z = self.PID_2[0] * error + self.PID_2[1] * self.iter_error_2 + self.PID_2[2] * delta_error
+        self.iter_error_2 += error
+        self.last_error_2 = error
+        z = max(min(z, 1), -1)
+
         return x, z
 
 
@@ -42,9 +64,9 @@ if __name__ == "__main__":
     rospy.init_node("home_edu_follower")
     rate = rospy.Rate(20)
     
-    f = Follower(PID=(0.002, 0.00001, 0.001))
+    f = Follower(PID_1=(0.001, 0.00001, 0.001), PID_2=(0.0015, 0.00003, 0.01))
     while not rospy.is_shutdown():
         x, z = f.next_step()
-        print(x, f.iter_error)
+        print(x, z)
         f.chassis.move(x, z)
         rate.sleep()
