4bba0bf9581e0ce4f733cdc4730cbcdc0d631a69
Kinda Lam <lamkinun@gmail.com>
2019-05-24 11:50:49 +0800

    add guass distance mask

--- scripts/core/Follower.py
+++ scripts/core/Follower.py
@@ -24,17 +24,30 @@ class Follower(object):
     def next_step(self):
         distance = 0
         offset_z = 0
+        offset_y = 0
         x = 0
         z = 0
+
+        gx, gy = np.meshgrid(np.linspace(-1, 1, 640), np.linspace(-1, 1, 480))
+        d = np.sqrt(gx * gx + gy * gy)
+        sigma, mu = 0.5, 0.0
+        g = np.exp(-((d - mu) ** 2 / (2.0 * sigma ** 2)))
+        print(np.min(g), np.max(g))
+        cv.imshow("g", g)
         
         depth_image = self.camera.depth_image
-        indices = np.nonzero(depth_image)
+        d = depth_image - (300 * g)
+
+        # indices = np.nonzero(depth_image)
+        indices = np.where(d > 1)
         if len(indices[0]) > 0:
-            distance = np.min(depth_image[indices])
-            n = np.where(depth_image == distance)
+            distance = np.min(d[indices])
+            n = np.where(d == distance)
             offset_z = n[1][0]
+            offset_y = n[0][0]
+            distance = depth_image[offset_y, offset_z]
         
-        if distance == 0 or distance > 1200:
+        if distance == 0 or distance > 1500:
             return x, z
         
         error = distance - self.target_distance
@@ -46,9 +59,10 @@ class Follower(object):
         self.iter_error_1 += error
         self.last_error_1 = error
         x = max(min(x, 0.5), -0.5)
+        print(error)
 
         error = 320 - offset_z
-        if abs(error) < 50:
+        if abs(error) < 10:
             error = 0
             self.iter_error_2 = 0
         delta_error = error - self.last_error_2
@@ -56,6 +70,12 @@ class Follower(object):
         self.iter_error_2 += error
         self.last_error_2 = error
         z = max(min(z, 1), -1)
+        print(error)
+
+        frame = self.camera.rgb_image
+        cv.circle(frame, (offset_z, offset_y), 10, (0, 0, 255), -1)
+        cv.imshow("frame", frame)
+        cv.waitKey(1)
 
         return x, z
 
@@ -64,7 +84,7 @@ if __name__ == "__main__":
     rospy.init_node("home_edu_follower")
     rate = rospy.Rate(20)
     
-    f = Follower(topic_name="camera_top", PID_1=(0.001, 0.00001, 0.001), PID_2=(0.0015, 0.00003, 0.01))
+    f = Follower(topic_name="camera_top", PID_1=(0.001, 0.0, 0.0), PID_2=(0.01, 0.0, 0.0))
     while not rospy.is_shutdown():
         x, z = f.next_step()
         print(x, z)
