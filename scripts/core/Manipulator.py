558b41e45bd06f7938c259153d33af8afef30990
Kinda Lam <lamkinun@gmail.com>
2019-05-27 15:46:35 +0800

    Manipulator

--- scripts/core/Manipulator.py
+++ scripts/core/Manipulator.py
@@ -41,7 +41,7 @@ class Manipulator(object):
     
     # Reset arm to default pose.
     def reset(self):
-        self.exec_servos_pos(10, 5, 0, 45)
+        self.exec_servos_pos(0, Manipulator.HAND_LENGTH + Manipulator.ARM_LENGTH * 2, 0, -90)
         self.close()
         self.wait()
         print("Manipulator is ready.")
